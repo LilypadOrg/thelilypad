40a203375b714cc8147d59ec874bb499336130a8
Shiv Bhonde <shivbhonde04@gmail.com>
2022-09-14 23:49:48 +0530

    add changes to [username].tsx

--- app/src/pages/profiles/[username].tsx
+++ app/src/pages/profiles/[username].tsx
@@ -1,94 +1,9 @@
 import { NextPage } from 'next';
-import Link from 'next/link';
-import Image from 'next/image';
 import React from 'react';
-import LevelPill from '../../components/ui/LevelPill';
-
-const languages = ['javascript', 'solidity', 'react', 'c++', 'python'];
-
-const InfoTile = ({
-  title,
-  tag,
-  linkTitle,
-  link,
-}: {
-  title: string;
-  tag: string;
-  linkTitle: string;
-  link: string;
-}) => {
-  return (
-    <div className="flex items-center justify-between  rounded-md border-2 border-main-gray-light py-3 px-4">
-      <div className="space-y-1">
-        <p className="text-lg font-bold">{title}</p>
-        <LevelPill level={tag} classes="bg-main-gray-light" />
-      </div>
-      <Link href={link}>
-        <a className="text-blue self-end underline">{linkTitle}</a>
-      </Link>
-    </div>
-  );
-};
-
-const LearningPathCards = ({
-  title,
-  img,
-  linkTitle,
-  link,
-}: {
-  title: string;
-  img: string;
-  linkTitle: string;
-  link: string;
-}) => {
-  return (
-    <div className="flex flex-col items-center space-y-4 bg-white p-4 shadow-lg">
-      <div className="flex items-center justify-between space-x-4">
-        <div className="h-6 w-8 rounded-full bg-green-600"></div>
-        <p className="text-md font-semibold leading-5">{title}</p>
-      </div>
-      <div className="my-4 h-24">
-        <Image src={img} alt="froggy" height={'100%'} width={'100%'} />
-      </div>
-      <Link href={link} className="">
-        {linkTitle}
-      </Link>
-    </div>
-  );
-};
-
-const paths = [1, 2, 3, 4, 5];
-
-const events = [
-  {
-    title: 'Hackathon: 4 Vs 1 Who is the best...',
-    id: '1',
-    time: 'Sun 11 september 2022',
-  },
-  {
-    title: 'Hackathon: 4 Vs 1 Who is the best...',
-    id: '2',
-    time: 'Sun 11 september 2022',
-  },
-];
-
-const projects = [
-  {
-    title: 'Hackathon: 4 Vs 1 Who is the best...',
-    id: 'a',
-    time: 'Sun 11 september 2022',
-  },
-  {
-    title: 'Hackathon: 4 Vs 1 Who is the best...',
-    id: 'b',
-    time: 'Sun 11 september 2022',
-  },
-  {
-    title: 'Hackathon: 4 Vs 1 Who is the best...',
-    id: 'c',
-    time: 'Sun 11 september 2022',
-  },
-];
+import { limitStrLength } from '~/utils/formatters';
+import { InfoTile, LearningPathCards } from '~/components/ui/userProfile';
+import CompletedIcon from '~/components/ui/CompletedIcon';
+import LevelPill from '~/components/ui/LevelPill';
 
 const UserProfile: NextPage = () => {
   return (
@@ -111,13 +26,11 @@ const UserProfile: NextPage = () => {
           </p>
         </div>
         <div className="space-y-3 bg-main-gray-light">
-          <Image
+          {/* Having hard time to use Image Component will fix this */}
+          <img
             src="/images/profileSBT/frogSBT.png"
             alt="sbt"
-            layout="intrinsic"
-            objectFit="contain"
-            width={500}
-            height={300}
+            className="w-ful h-full"
           />
           <button className="w-full rounded-[6.5px] bg-primary-400 px-10 py-4 font-bold text-white">
             Mint Your SBT
@@ -181,8 +94,172 @@ const UserProfile: NextPage = () => {
           ))}
         </div>
       </div>
+      {/* My personal roadmap */}
+      <div className="flex flex-col px-[5.5rem] py-[2.2rem]">
+        <h1 className="mb-1 text-3xl font-bold">My Personal Roadmap</h1>
+        <p className="w-[40%] font-light">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita
+          quis rem soluta maxime. Dolor qui inventore .
+        </p>
+      </div>
+      {/* Beginner */}
+      <div className="mt-14 flex justify-between border-t-2 border-main-gray-dark px-[5.5rem] py-12">
+        <div className="flex items-center space-x-14">
+          <div className="flex items-center justify-between space-x-4">
+            <CompletedIcon />
+            <p className="text-md font-semibold leading-5 underline">
+              Beginner 3/3
+            </p>
+          </div>
+          <LevelPill
+            level={'Completed on August 12 2022'}
+            classes="bg-main-gray-light mt-2"
+          />
+        </div>
+        <button className="rounded-md bg-gray-800 px-10 py-2 font-semibold text-white">
+          More Beginner Roadmap
+        </button>
+      </div>
+      {/* Intermediate */}
+      <div className="flex flex-col space-y-10 border-t-2 border-main-gray-dark px-[5.5rem] py-12">
+        <div className="flex justify-between space-x-8">
+          <div className="flex flex-col space-y-6">
+            <p className="text-md font-semibold leading-5 underline">
+              Intermediate 1/3
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
+              ipsam praesentium esse!
+            </p>
+            <button className="self-start rounded-md bg-main-gray-light px-12 py-2 font-semibold">
+              Take final test
+            </button>
+          </div>
+          <div className="mt-14 flex space-x-4">
+            {interMediateCourses.map((course, i) => (
+              /* Its same as CourseCard component */
+              <div
+                className="min-w-[20rem]  rounded-lg shadow-lg"
+                key={`${course.title}-${i}`}
+              >
+                <div className="relative h-[182px]  w-full rounded-tr-lg rounded-tl-lg bg-main-gray-dark"></div>
+                <div className="flex items-center space-x-3 px-4 pt-4">
+                  {course.isCompleted && <CompletedIcon />}
+                  <LevelPill level={course.level} classes="mt-2" />
+                </div>
+                <div className="flex flex-col justify-between px-4 py-3">
+                  <div className="mb-2 text-lg font-bold">{course.title}</div>
+                  <div className=" text-ellipsis text-base text-gray-700">
+                    {limitStrLength(course.description, 80)}
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <button className="ml-auto rounded-md bg-gray-800 px-10 py-2 font-semibold text-white">
+          More Intermediate Roadmap
+        </button>
+      </div>
+      {/* Advanced */}
+      <div className="flex flex-col space-y-10 border-t-2 border-main-gray-dark px-[5.5rem] py-12">
+        <div className="flex justify-between space-x-8">
+          <div className="flex flex-col space-y-6">
+            <p className="text-md font-semibold leading-5 underline">
+              Advanced 0/3
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
+              ipsam praesentium esse!
+            </p>
+            <button className="self-start rounded-md bg-main-gray-light px-12 py-2 font-semibold">
+              Take final test
+            </button>
+          </div>
+          <div className="mt-14 flex space-x-4">
+            {interMediateCourses.map((course, i) => (
+              /* Its same as CourseCard component */
+              <div
+                className="min-w-[20rem]  rounded-lg shadow-lg"
+                key={`${course.title}-${i}`}
+              >
+                <div className="relative h-[182px]  w-full rounded-tr-lg rounded-tl-lg bg-main-gray-dark"></div>
+
+                <div className="flex items-center space-x-3 px-4 pt-4">
+                  <LevelPill level={'Advanced'} classes="mt-2" />
+                </div>
+                <div className="flex flex-col justify-between px-4 py-3">
+                  <div className="mb-2 text-lg font-bold">{course.title}</div>
+                  <div className=" text-ellipsis text-base text-gray-700">
+                    {limitStrLength(course.description, 80)}
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <button className="ml-auto rounded-md bg-gray-800 px-10 py-2 font-semibold text-white">
+          More Intermediate Roadmap
+        </button>
+      </div>
     </div>
   );
 };
+
+const languages = ['javascript', 'solidity', 'react', 'c++', 'python'];
+
+const interMediateCourses = [
+  {
+    level: 'intermediate',
+    isCompleted: false,
+    title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    description: 'Lorem ipsum dolor sit amet consectetur.',
+  },
+  {
+    level: 'intermediate',
+    isCompleted: false,
+    title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    description: 'Lorem ipsum dolor sit amet consectetur.',
+  },
+  {
+    level: 'intermediate',
+    isCompleted: true,
+    title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    description: 'Lorem ipsum dolor sit amet consectetur.',
+  },
+];
+
+const paths = [1, 2, 3, 4, 5];
+
+const events = [
+  {
+    title: 'Hackathon: 4 Vs 1 Who is the best...',
+    id: '1',
+    time: 'Sun 11 september 2022',
+  },
+  {
+    title: 'Hackathon: 4 Vs 1 Who is the best...',
+    id: '2',
+    time: 'Sun 11 september 2022',
+  },
+];
+
+const projects = [
+  {
+    title: 'Hackathon: 4 Vs 1 Who is the best...',
+    id: 'a',
+    time: 'Sun 11 september 2022',
+  },
+  {
+    title: 'Hackathon: 4 Vs 1 Who is the best...',
+    id: 'b',
+    time: 'Sun 11 september 2022',
+  },
+  {
+    title: 'Hackathon: 4 Vs 1 Who is the best...',
+    id: 'c',
+    time: 'Sun 11 september 2022',
+  },
+];
 
 export default UserProfile;
