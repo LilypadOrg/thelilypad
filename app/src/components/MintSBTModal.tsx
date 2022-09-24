d1cbd0f506ee259e6803cdd79da4742a522d7ae2
Shiv Bhonde <shivbhonde04@gmail.com>
2022-09-25 00:40:56 +0530

    close mintSBTModal on outside click and Esc

--- app/src/components/MintSBTModal.tsx
+++ app/src/components/MintSBTModal.tsx
@@ -1,9 +1,11 @@
 // import ContractWriteFn from 'wagmi/dist/declarations/src/hooks//contracts/useContractWrite';
 
 import Image from 'next/image';
+import React, { useCallback, useEffect, useRef } from 'react';
 import Tilt from 'react-parallax-tilt';
 
 const MintSBTModal = ({
+  open,
   closeModal,
   mintFunction,
   mintIsLoading,
@@ -20,8 +22,35 @@ const MintSBTModal = ({
     }
   };
 
+  const modalRef = useRef(null);
+
+  const hideModal = (e: React.MouseEvent<HTMLElement>) => {
+    if (modalRef.current === e.target) {
+      closeModal();
+    }
+  };
+
+  const keyPress = useCallback(
+    (e: KeyboardEvent) => {
+      if (e.key === 'Escape' && open) {
+        closeModal();
+        console.log('Esc pressed');
+      }
+    },
+    [closeModal, open]
+  );
+
+  useEffect(() => {
+    document.addEventListener('keydown', keyPress);
+    return () => document.removeEventListener('keydown', keyPress);
+  }, [keyPress]);
+
   return (
-    <div className="fixed top-0 left-0 z-50 flex h-full w-full items-center justify-center overflow-auto bg-[rgba(0,0,0,0.5)] p-1">
+    <div
+      className="fixed top-0 left-0 z-50 flex h-full w-full items-center justify-center overflow-auto bg-[rgba(0,0,0,0.5)] p-1"
+      ref={modalRef}
+      onClick={hideModal}
+    >
       <div className="min animate-fade-in-down relative w-7/12 rounded-2xl bg-secondary-400 p-5">
         <div className="flex flex-row items-center space-x-4 text-gray-800">
           <Tilt>
