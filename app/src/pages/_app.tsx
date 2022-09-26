cdc4d91ec2a6ff8a020370c2288b5ec7c716f4c2
Andrea C <mirthis@gmail.com>
2022-09-26 15:34:22 +0100

    revert api url change

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -45,11 +45,11 @@ export default withTRPC<AppRouter>({
      * If you want to use SSR, you need to use the server's full URL
      * @link https://trpc.io/docs/ssr
      */
-    const url = 'https://thelilypad.vercel.app//api/trpc';
+    // const url = 'https://thelilypad.vercel.app//api/trpc';
 
-    // const url = process.env.VERCEL_URL
-    //   ? `https://${process.env.VERCEL_URL}/api/trpc`
-    //   : 'http://localhost:3000/api/trpc';
+    const url = process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}/api/trpc`
+      : 'http://localhost:3000/api/trpc';
     return {
       url,
       transformer: superjson,
