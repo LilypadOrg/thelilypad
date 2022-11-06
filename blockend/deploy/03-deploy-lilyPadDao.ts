4668b93c15813763b8a7262afe3257b4e47c3dee
Marcos Vinícius Correa Rodrigues <rodrigues.mvinicius@gmail.com>
2022-11-06 14:48:48 -0300

    DAO Main Contract Correction (Should be ILilyPad and not IMain) DAO deploy corrections (passing parameters to constructor) DAO test scripts

--- blockend/deploy/03-deploy-lilyPadDao.ts
+++ blockend/deploy/03-deploy-lilyPadDao.ts
@@ -1,6 +1,8 @@
 import { ethers, network, upgrades } from "hardhat";
 import { DeployFunction } from "hardhat-deploy/dist/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { networkConfig } from "../helper-hardhat.config";
+import { burnAddress, currentNetWork } from "../scripts/utils";
 import { LilyPadExecutor__factory, LilyPadGovernor__factory } from "../typechain-types";
 
 const deployLilyPadDao: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
@@ -10,7 +12,7 @@ const deployLilyPadDao: DeployFunction = async (hre: HardhatRuntimeEnvironment) 
     const { deployer, safeCaller } = await getNamedAccounts();
     const { chainId } = network.config;
 
-    const minDelay = 500;
+    const minDelay = networkConfig[currentNetWork()].executorMinDelay;
     const proposers: any[] = [];
     const executors: any[] = [];
 
@@ -24,6 +26,8 @@ const deployLilyPadDao: DeployFunction = async (hre: HardhatRuntimeEnvironment) 
         executors,
     ]);
     await executorContract.deployed();
+
+    const executorInstance = await executorFactory.attach(executorContract.address);
 
     console.log(`LilyPad Executor (Proxy) deployed to ${executorContract.address}`);
     console.log(
@@ -39,18 +43,24 @@ const deployLilyPadDao: DeployFunction = async (hre: HardhatRuntimeEnvironment) 
     const sbtContract = await get("PondSBT");
     const mainContract = await get("LilyPad");
 
+    console.log(`Main Contract: ${mainContract.address}`);
+
     const governorFactory: LilyPadGovernor__factory = await ethers.getContractFactory(
         "LilyPadGovernor"
     );
     const governorContract = await upgrades.deployProxy(governorFactory, [
         sbtContract.address,
         executorContract.address,
-        2,
+        networkConfig[currentNetWork()].votingDelay,
+        networkConfig[currentNetWork()].votingPeriod,
+        networkConfig[currentNetWork()].levelThreshold,
         mainContract.address,
         deployer,
     ]);
 
     await governorContract.deployed();
+
+    const governorInstance = await governorFactory.attach(governorContract.address);
 
     console.log(`LilyPad Governor (Proxy) deployed to ${governorContract.address}`);
     console.log(
@@ -61,6 +71,18 @@ const deployLilyPadDao: DeployFunction = async (hre: HardhatRuntimeEnvironment) 
         await upgrades.erc1967.getAdminAddress(governorContract.address),
         " getAdminAddress"
     );
+
+    const proposer_role = await executorInstance.PROPOSER_ROLE();
+    const executor_role = await executorInstance.EXECUTOR_ROLE();
+    const timelock_admin_role = await executorInstance.TIMELOCK_ADMIN_ROLE();
+    const canceler_role = await executorInstance.CANCELLER_ROLE();
+    await executorInstance.grantRole(proposer_role, governorInstance.address);
+    await executorInstance.grantRole(canceler_role, governorInstance.address);
+    await executorInstance.grantRole(executor_role, burnAddress);
+    const tx = await executorInstance.revokeRole(timelock_admin_role, deployer);
+    await tx.wait(1);
+
+    console.log("Guess what? Now you can't do anything!");
 
     const executorArtifact = await deployments.getArtifact("LilyPadExecutor");
     const governorArtifact = await deployments.getArtifact("LilyPadGovernor");
