91656b0fb20f7b1dee3503a276e0c2a864783b75
Andrea C <mirthis@gmail.com>
2022-11-27 15:01:06 +0000

    remove unused code in useOnchainProfile hook

--- app/src/hooks/useOnChainProfile.tsx
+++ app/src/hooks/useOnChainProfile.tsx
@@ -1,13 +1,13 @@
-import { BigNumber } from 'ethers';
+// import { BigNumber } from 'ethers';
 import { useContractRead } from 'wagmi';
-import { OnChainProfile } from '~/types/types';
+// import { OnChainProfile } from '~/types/types';
 import {
   getLilyPadABI,
   getLilyPadAddress,
-  getPondSBTABI,
-  getPondSBTAddress,
+  // getPondSBTABI,
+  // getPondSBTAddress,
 } from '~/utils/contracts';
-import { trpc } from '~/utils/trpc';
+// import { trpc } from '~/utils/trpc';
 
 export const useOnChainProfile = (address: string | undefined) => {
   const {
@@ -20,43 +20,45 @@ export const useOnChainProfile = (address: string | undefined) => {
     functionName: 'getMember',
     enabled: !!address,
     args: [address as `0x${string}`],
-    cacheTime: 0,
-    staleTime: 0,
   });
 
-  const { data: tokenUri, isLoading: isLoadingTokenURI } = useContractRead({
-    address: getPondSBTAddress(),
-    abi: getPondSBTABI(),
-    functionName: 'tokenURI',
-    enabled: !!onChainProfile && onChainProfile.tokenId._hex !== '0x00',
-    args: [onChainProfile?.tokenId || BigNumber.from('0x00')],
-  });
+  // const { data: tokenUri, isLoading: isLoadingTokenURI } = useContractRead({
+  //   address: getPondSBTAddress(),
+  //   abi: getPondSBTABI(),
+  //   functionName: 'tokenURI',
+  //   enabled: !!onChainProfile && onChainProfile.tokenId._hex !== '0x00',
+  //   args: [onChainProfile?.tokenId || BigNumber.from('0x00')],
+  // });
 
-  const { data: tokenMetadata, isFetching: isLoadingTokenMetadata } =
-    trpc.useQuery(
-      ['blockend.getTokenMetadata', { tokenUri: tokenUri?.toString() || '' }],
-      {
-        enabled: !!tokenUri,
-      }
-    );
+  // const { data: tokenMetadata, isFetching: isLoadingTokenMetadata } =
+  //   trpc.useQuery(
+  //     ['blockend.getTokenMetadata', { tokenUri: tokenUri?.toString() || '' }],
+  //     {
+  //       enabled: !!tokenUri,
+  //     }
+  //   );
 
-  const isLoading =
-    isLoadingOnChainProfile || isLoadingTokenMetadata || isLoadingTokenURI;
+  // const isLoading =
+  //   isLoadingOnChainProfile || isLoadingTokenMetadata || isLoadingTokenURI;
 
-  const data: OnChainProfile | undefined = onChainProfile
-    ? {
-        DAO: !!onChainProfile?.DAO,
-        pathChosen: !!onChainProfile?.pathChosen,
-        xp: onChainProfile?.xp,
-        level: onChainProfile?.level,
-        tokenId: onChainProfile?.tokenId,
-        tokenUri: tokenUri?.toString(),
-        tokenMetadata,
-        sbtImageUrl: tokenMetadata?.image
-          .replace('ipfs:', 'https:')
-          .concat('.ipfs.nftstorage.link/'),
-      }
-    : undefined;
+  // const data: OnChainProfile | undefined = onChainProfile
+  //   ? {
+  //       DAO: !!onChainProfile?.DAO,
+  //       pathChosen: !!onChainProfile?.pathChosen,
+  //       xp: onChainProfile?.xp,
+  //       level: onChainProfile?.level,
+  //       tokenId: onChainProfile?.tokenId,
+  //       tokenUri: tokenUri?.toString(),
+  //       tokenMetadata,
+  //       sbtImageUrl: tokenMetadata?.image
+  //         .replace('ipfs:', 'https:')
+  //         .concat('.ipfs.nftstorage.link/'),
+  //     }
+  //   : undefined;
 
-  return { data, refetchProfile, isLoading };
+  return {
+    data: onChainProfile,
+    refetchProfile,
+    isLoading: isLoadingOnChainProfile,
+  };
 };
