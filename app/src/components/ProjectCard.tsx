218f64ab1ce77515335cef52af9fda0333b46243
Shiv Bhonde <shivbhonde04@gmail.com>
2023-02-27 01:05:25 +0530

    update home page inline with new design

--- app/src/components/ProjectCard.tsx
+++ app/src/components/ProjectCard.tsx
@@ -16,11 +16,11 @@ export const SpotLightCards = ({
   console.log(`${PROJECTS_IMAGE_PATH}${coverImageUrl}`);
 
   return (
-    <>
+    <div className="cursor-pointer">
       <Link href={`/projects/${id}/${slug}`}>
         <div
           key={`featured-${title}`}
-          className="relative flex items-center justify-center rounded-lg border-[0.1rem] border-primary-600 bg-primary-600"
+          className="primary-shadow relative flex items-center justify-center border-[0.1rem] border-primary-600 bg-primary-600 shadow-[10px_10px_1px_#7B61FF] transition-all hover:shadow-[-1px_-1px_1px_#7B61FF]"
         >
           {coverImageUrl && (
             <Image
@@ -28,7 +28,6 @@ export const SpotLightCards = ({
               alt={`${title} thumbnail`}
               height={320}
               width={420}
-              className="rounded-lg transition-all hover:scale-95"
             />
           )}
           <div
@@ -46,6 +45,6 @@ export const SpotLightCards = ({
           </div>
         </div>
       </Link>
-    </>
+    </div>
   );
 };
